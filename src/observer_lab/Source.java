fcd4a63b2021a9c4737913d471cd8f134a03efef
Bananananah <111550439+Bananananah@users.noreply.github.com>
2022-09-23 20:48:57 +0700

    up

---
+++ src/observer_lab/Source.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package observer_lab;
+
+public interface Source {
+    public void notifyObserver();
+    public void register(MyObservers observer);
+}
